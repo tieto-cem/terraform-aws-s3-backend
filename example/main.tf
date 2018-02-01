6734b1c110969517ac39b35592594ab71412e1a3
timotapanainen@users.noreply.github.com <timo.tapanainen@gmail.com>
2018-02-01 20:37:15 +0200

    use relative source reference in examples

--- example/main.tf
+++ example/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "terraform_s3_backend" {
-  source                   = "github.com/timotapanainen/terraform-aws-s3-backend?ref=v0.1.1"
+  source                   = ".."
   s3_state_bucket_name     = "myapp-terraform-state-bucket"
   dynamodb_lock_table_name = "myapp-terraform-state-lock-table"
 }
