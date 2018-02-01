1d9cec1504f062101ec7fd4cf7a851c94a88d468
timotapanainen@users.noreply.github.com <timo.tapanainen@gmail.com>
2018-02-01 16:22:51 +0200

    updated source reference

--- example/main.tf
+++ example/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "terraform_s3_backend" {
-  source                   = "github.com/tieto-cem/terraform-aws-s3-backend?ref=v0.1.0"
+  source                   = "github.com/timotapanainen/terraform-aws-s3-backend?ref=v0.1.1"
   s3_state_bucket_name     = "myapp-terraform-state-bucket"
   dynamodb_lock_table_name = "myapp-terraform-state-lock-table"
 }
